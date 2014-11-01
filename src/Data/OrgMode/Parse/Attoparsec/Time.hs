e3848348561eba4e0cc0ed60fc6d943affd04113
Parnell Springmeyer <parnell@ixmat.us>
2014-11-01 16:11:15 -0500

    Adding in time and scheduled parsers

--- src/Data/OrgMode/Parse/Attoparsec/Time.hs
+++ src/Data/OrgMode/Parse/Attoparsec/Time.hs
@@ -15,17 +15,39 @@
 module Data.OrgMode.Parse.Attoparsec.Time
 ( module Data.OrgMode.Parse.Internal
 , timestamp
-, agenda
+, scheduled
 )
 where
 
-import           Control.Applicative         ((*>), (<*), (<|>))
+import           Control.Applicative         ((*>), (<$>), (<*), (<|>))
 import           Data.Attoparsec.Text        as T
 import           Data.Attoparsec.Types       as TP (Parser)
 import           Data.Char                   (isUpper)
 import           Data.Maybe                  (catMaybes, isJust)
 import           Data.Text                   as Text (Text, concat, length,
                                                       null, pack)
+import           Data.Thyme.Format           (buildTime, timeParser)
+import           Data.Thyme.LocalTime        (LocalTime (..))
 import           Prelude                     hiding (concat, null, takeWhile)
+import           System.Locale               (defaultTimeLocale,
+                                              iso8601DateFormat)
 
 import           Data.OrgMode.Parse.Internal
+
+activeTimestamp :: TP.Parser Text LocalTime
+activeTimestamp = do
+    s'    <- skipSpace *> scheduled
+    stamp <- skipSpace *> timeParser defaultTimeLocale formatString
+    return $ buildTime stamp
+  where
+    formatString = "%Y-%m-%d %a"
+
+scheduled :: TP.Parser Text (Maybe Scheduled)
+scheduled = do
+    sd <- option Nothing sched <* char ':'
+    return $ case sd of
+        Just "DEADLINE"  -> Just DEADLINE
+        Just "SCHEDULED" -> Just SCHEDULED
+        Nothing          -> Nothing
+  where
+    sched = Just <$> (string "DEADLINE" <|> string "SCHEDULED")
