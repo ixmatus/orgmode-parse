f403a4cdb935e1f5691591457d8d96ece5046702
Parnell Springmeyer <parnell@digitalmentat.com>
2015-04-12 02:10:18 -0500

    Cleaning up some of the functions and making them easier to read

--- src/Data/OrgMode/Parse/Attoparsec/Time.hs
+++ src/Data/OrgMode/Parse/Attoparsec/Time.hs
@@ -14,22 +14,21 @@
 
 module Data.OrgMode.Parse.Attoparsec.Time where
 
-import           Control.Applicative        ((<*>), (*>), (<$>), (<*),
-                                             (<|>), pure)
+import           Control.Applicative        (pure, (*>), (<$>), (<*), (<*>),
+                                             (<|>))
 import           Control.Monad              (when)
 import qualified Data.Attoparsec.ByteString as AB
 import           Data.Attoparsec.Text       as T
 import           Data.Attoparsec.Types      as TP (Parser)
 import qualified Data.ByteString.Char8      as BS
-import           Data.HashMap.Strict        (HashMap,fromList)
+import           Data.HashMap.Strict        (HashMap, fromList)
 import           Data.Maybe                 (listToMaybe)
-import           Data.Text                  as Text (Text, unwords,
-                                                     pack, unpack )
+import           Data.Text                  as Text (Text, pack, unpack,
+                                                     unwords)
 import           Data.Thyme.Format          (buildTime, timeParser)
 import           Data.Thyme.LocalTime       (Hours, Minutes)
-import           Prelude                    hiding (concat, null,
-                                                    takeWhile,
-                                                    unwords, words)
+import           Prelude                    hiding (concat, null, takeWhile,
+                                             unwords, words)
 import           System.Locale              (defaultTimeLocale)
 
 import           Data.OrgMode.Parse.Types
@@ -51,29 +50,37 @@ parsePlannings = fromList <$> (many' (planning <* skipSpace))
 
 -- | Parse a clock line
 --
--- A heading's section contains one line per clock entry
--- Clocks may have a timestamp, a duration, both, or neither
--- e.g.: CLOCK: [2014-12-10 Fri 2:30]--[2014-12-10 FRI 10:30]  => 08:00
+-- A heading's section contains one line per clock entry. Clocks may
+-- have a timestamp, a duration, both, or neither e.g.:
+--
+-- > CLOCK: [2014-12-10 Fri 2:30]--[2014-12-10 Fri 10:30] => 08:00
 parseClock :: TP.Parser Text (Maybe Timestamp, Maybe Duration)
 parseClock = (,) <$> (skipSpace *> string "CLOCK: " *> ts) <*> dur
-  where ts  = option Nothing (Just <$> parseTimestamp)
-        dur = option Nothing (Just <$> (string " => "
-                              *> skipSpace *> parseHM))
+  where
+    ts  = option Nothing (Just <$> parseTimestamp)
+    dur = option Nothing (Just <$> (string " => "
+                                    *> skipSpace *> parseHM))
 
 -- | Parse a timestamp
 --
 -- Timestamps may be timepoints or timeranges, and they indicate
--- whether they are active or closed by using angle or square brackets respectively
+-- whether they are active or closed by using angle or square brackets
+-- respectively.
 --
--- Time ranges are formatted by appending two timepoints with '--' in between,
---   or by appending two hh:mm stamps together in a single timepoint with a single '-'
+-- Time ranges are formatted by infixing two timepoints with a double
+-- hyphen, @--@; or, by appending two @hh:mm@ timestamps together in a
+-- single timepoint with one hyphen @-@.
 --
--- Each timepoint includes an optional repeater flag and an optional delay flag
+-- Each timepoint includes an optional repeater flag and an optional
+-- delay flag.
 parseTimestamp :: TP.Parser Text Timestamp
 parseTimestamp = do
   (ts1,ts1b,active1) <- parseBracketedDateTime
+
   t2 <- option Nothing (Just <$> (string "--" *> parseBracketedDateTime))
+
   case (ts1b,t2) of
+
     (Nothing,      Nothing) ->
        return (Timestamp ts1 active1 Nothing)
     (Nothing, Just (ts2,Nothing,_)) ->
@@ -100,10 +107,13 @@ parseBracketedDateTime :: TP.Parser Text (DateTime, Maybe (Hours,Minutes), Bool)
 parseBracketedDateTime = do
   oBracket   <- char '<' <|> char '['
   datePart   <- parseDate                            <* skipSpace
+
   dName      <- option Nothing (Just <$> parseDay)   <* skipSpace
   timePart   <- option Nothing (Just <$> parseTime') <* skipSpace
+
   repeatPart <- listToMaybe <$> many' parseRepeater  <* skipSpace
   delayPart  <- listToMaybe <$> many' parseDelay     <* skipSpace
+
   cBracket   <- char '>' <|> char ']'
 
   when (complementaryBracket oBracket /= cBracket)
@@ -130,52 +140,56 @@ parseBracketedDateTime = do
 
 -- | Parse a 3-character day name
 parseDay :: TP.Parser Text Text
-parseDay = choice (map string ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"])
+parseDay = choice (map string ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"])
 
 -- | Parse the time-of-day part of a time part, as a single point or a time range
 parseTime' :: TP.Parser Text (Either (Hours,Minutes) ((Hours,Minutes),(Hours,Minutes)))
-parseTime' = choice [Right <$> ((,) <$> parseHM <* char '-' <*> parseHM)
-                    ,Left  <$> parseHM ]
+parseTime' =
+    choice [ Right <$> ((,) <$> parseHM <* char '-' <*> parseHM)
+           , Left  <$> parseHM
+           ]
 
-
--- | Parse the YYYY-MM-DD part of a time part
+-- | Parse the YYYY-MM-DD part of a time part.
 parseDate :: TP.Parser Text YearMonthDay
-parseDate = manyTill anyChar (char ' ') >>= \dStr ->
-  case AB.parseOnly dParser (BS.pack dStr) of
-    Left e      -> fail .unpack $
-                   unwords ["No time parse for: ", pack dStr, " Message:", pack e]
-    Right lTime -> return (buildTime lTime)
-  where dParser = timeParser defaultTimeLocale "%Y-%m-%d"
+parseDate = consumeDate >>= either failure success . dateParse
+  where
+    failure e = fail . unpack $ unwords ["Failure parsing date: ", pack e]
+    success t = return $ buildTime t
+    consumeDate  = manyTill anyChar (char ' ')
+    dateParse    = AB.parseOnly dpCombinator . BS.pack
+    dpCombinator = timeParser defaultTimeLocale "%Y-%m-%d"
 
--- | Parse a single HH:MM point
-parseHM :: TP.Parser Text (Hours,Minutes)
+-- | Parse a single @HH:MM@ point.
+parseHM :: TP.Parser Text (Hours, Minutes)
 parseHM = (,) <$> decimal <* char ':' <*> decimal
 
--- Parse the Timeunit part of a delay or repeater flag
+-- | Parse the Timeunit part of a delay or repeater flag.
 parseTimeUnit :: TP.Parser Text TimeUnit
-parseTimeUnit = choice [char 'h' *> pure UnitHour
-                       ,char 'd' *> pure UnitDay
-                       ,char 'w' *> pure UnitWeek
-                       ,char 'm' *> pure UnitMonth
-                       ,char 'y' *> pure UnitYear]
+parseTimeUnit =
+    choice [ char 'h' *> pure UnitHour
+           , char 'd' *> pure UnitDay
+           , char 'w' *> pure UnitWeek
+           , char 'm' *> pure UnitMonth
+           , char 'y' *> pure UnitYear
+           ]
 
-
--- | Parse a repeater flag, e.g. ".+4w", or "++1y"
+-- | Parse a repeater flag, e.g. @.+4w@, or @++1y@.
 parseRepeater :: TP.Parser Text Repeater
-parseRepeater = Repeater
-                <$> choice[string "++" *> pure RepeatCumulate
-                          ,string "+"  *> pure RepeatCatchUp
-                          ,string ".+" *> pure RepeatRestart
-                          ]
-                <*> decimal
-                <*> parseTimeUnit
+parseRepeater =
+    Repeater
+    <$> choice[ string "++" *> pure RepeatCumulate
+              , char   '+'  *> pure RepeatCatchUp
+              , string ".+" *> pure RepeatRestart
+              ]
+    <*> decimal
+    <*> parseTimeUnit
 
--- | Parse a delay flag, e.g. "--1d" or "-2w"
+-- | Parse a delay flag, e.g. @--1d@ or @-2w@.
 parseDelay :: TP.Parser Text Delay
-parseDelay = Delay
-             <$> choice [string "--" *> pure DelayFirst
-                        ,string "-"  *> pure DelayAll
-                        ]
-             <*> decimal
-             <*> parseTimeUnit
-
+parseDelay =
+    Delay
+    <$> choice [ string "--" *> pure DelayFirst
+               , char '-'    *> pure DelayAll
+               ]
+    <*> decimal
+    <*> parseTimeUnit
