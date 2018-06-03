32ed0462e0762684a9ad5133bc265adb8a164e0b
Parnell Springmeyer <parnell@digitalmentat.com>
2018-06-03 18:37:59 -0500

    Pin nixpkgs to 17.09, supply a shell.nix for development convenience

--- release.nix
+++ release.nix
@@ -1,20 +1,23 @@
-let config = {
-  allowUnfree = true;
-  packageOverrides = pkgs: {
-    haskellPackages = pkgs.haskellPackages.override {
-      overrides = haskellPackagesNew: haskellPackagesOld: {
-        orgmode-parse = haskellPackagesNew.callPackage ./default.nix { };
-      };
-    };
-  };
-};
-in
-
-{ pkgs ? import <nixpkgs> { inherit config; } }:
 let
-  darwinPkgs = import <nixpkgs> { inherit config; system = "x86_64-darwin"; };
-  linuxPkgs  = import <nixpkgs> { inherit config; system = "x86_64-linux" ; };
-  pkgs       = import <nixpkgs> { inherit config; };
+
+  config   = { allowUnfree = true; };
+  overlays = [
+    (newPkgs: oldPkgs: rec {
+
+      haskellPackages = oldPkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: {
+          orgmode-parse = haskellPackagesNew.callPackage ./default.nix { };
+        };
+      };
+
+    })
+  ];
+
+  nixpkgs = import ./nix/17_09.nix;
+  pkgs    = import nixpkgs { inherit config overlays; };
+
+  darwinPkgs = import nixpkgs { inherit config overlays; system = "x86_64-darwin"; };
+  linuxPkgs  = import nixpkgs { inherit config overlays; system = "x86_64-linux" ; };
 
 in
   { orgmode-parse-linux  =  linuxPkgs.haskellPackages.orgmode-parse;
