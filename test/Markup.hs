ab126d4aad709c01c14681957329f04b705f9421
Ben Sima <ben@bsima.me>
2018-06-14 14:21:06 -0700

    Remove unnecessary imports

--- test/Markup.hs
+++ test/Markup.hs
@@ -2,17 +2,9 @@
 
 module Markup where
 
-import           Data.Attoparsec.Text
-import           Data.Text
-import qualified Data.Text                              as Text
-import qualified Data.Text.IO                           as TextIO
 import           Test.Tasty
 import           Test.Tasty.HUnit
-import           Data.HashMap.Strict
-
 import           Data.OrgMode.Parse.Attoparsec.Markup
-import           Data.OrgMode.Parse.Attoparsec.Time
-import           Data.OrgMode.Types
 import           Util
 
 
