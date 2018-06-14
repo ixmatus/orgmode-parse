8aa5cc0268d719faa6c404dfd0b07dac810b2693
Ben Sima <ben@bsima.me>
2018-06-14 14:21:47 -0700

    Parse Underline, Monospace, Code, & Strike

--- src/Data/OrgMode/Parse/Attoparsec/Markup.hs
+++ src/Data/OrgMode/Parse/Attoparsec/Markup.hs
@@ -15,28 +15,43 @@ import           Data.Text                              (Text)
 data Markup
   = Bold Text
   | Italic Text
-  -- | Underline
-  -- | Monospace
-  -- | Code
-  -- | Strike
+  | Underline Text
+  | Monospace Text
+  | Code Text
+  | Strike Text
   deriving (Show, Eq)
 
 parseMarkup :: Attoparsec.Parser Text Markup
 parseMarkup = choice
   [ parseBold
   , parseItalic
+  , parseUnderline
+  , parseMonospace
+  , parseCode
+  , parseStrike
   ]
 
+parseWrap :: (Text -> Markup) -> Char -> Attoparsec.Parser Text Markup
+parseWrap t c = do
+  _ <- char c
+  content <- takeTill (== c)
+  _ <- char c
+  return $ t content
+
 parseItalic :: Attoparsec.Parser Text Markup
-parseItalic = do
-  _ <- char '/'
-  content <- takeTill (== '/')
-  _ <- char '/'
-  return $ Italic content
+parseItalic = parseWrap Italic '/'
 
 parseBold :: Attoparsec.Parser Text Markup
-parseBold = do
-  _ <- char '*'
-  content <- takeTill (== '*')
-  _ <- char '*'
-  return $ Bold content
+parseBold = parseWrap Bold '*'
+
+parseUnderline :: Attoparsec.Parser Text Markup
+parseUnderline = parseWrap Underline '_'
+
+parseMonospace :: Attoparsec.Parser Text Markup
+parseMonospace = parseWrap Monospace '='
+
+parseCode :: Attoparsec.Parser Text Markup
+parseCode = parseWrap Code '~'
+
+parseStrike :: Attoparsec.Parser Text Markup
+parseStrike = parseWrap Strike '+'
--- test/Markup.hs
+++ test/Markup.hs
@@ -14,6 +14,14 @@ parserMarkupTests = testGroup "Attoparsec bold text"
       testDocS "*bold*" (Bold "bold")
   , testCase "Parses italic words" $
       testDocS "/italic words/" (Italic "italic words")
+  , testCase "Parses underlined text" $
+      testDocS "_underlined stuff_" (Underline "underlined stuff")
+  , testCase "Parses monospace text" $
+      testDocS "=verbatim=" (Monospace "verbatim")
+  , testCase "Parses code" $
+      testDocS "~some code~" (Code "some code")
+  , testCase "Parses strikethrough" $
+      testDocS "+strike this+" (Strike "strike this")
   ]
   where
     testDocS s r = expectParse parseMarkup s (Right r)
