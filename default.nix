df640a909f57888625038dd06a199991f312a2a9
Parnell Springmeyer <ixmatus@users.noreply.github.com>
2018-10-10 21:57:32 -0500

    Bumping orgmode-parse to 0.2.2 for release to hackage (#44)

--- default.nix
+++ default.nix
@@ -1,19 +1,19 @@
 { mkDerivation, aeson, attoparsec, base, bytestring, containers
-, free, hashable, HUnit, neat-interpolation, old-locale, stdenv
-, tasty, tasty-hunit, text, thyme, unordered-containers
+, free, hashable, HUnit, neat-interpolation, old-locale, semigroups
+, stdenv, tasty, tasty-hunit, text, thyme, unordered-containers
 }:
 mkDerivation {
   pname = "orgmode-parse";
-  version = "0.1.1.3";
+  version = "0.2.2";
   src = ./.;
   libraryHaskellDepends = [
     aeson attoparsec base bytestring containers free hashable
-    old-locale text thyme unordered-containers
+    old-locale semigroups text thyme unordered-containers
   ];
   testHaskellDepends = [
     aeson attoparsec base bytestring containers free hashable HUnit
-    neat-interpolation old-locale tasty tasty-hunit text thyme
-    unordered-containers
+    neat-interpolation old-locale semigroups tasty tasty-hunit text
+    thyme unordered-containers
   ];
   description = "A collection of Attoparsec combinators for parsing org-mode flavored documents";
   license = stdenv.lib.licenses.bsd3;
