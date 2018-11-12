42d7091687b6a4c89290a32b552d1a3342fc7fc1
Parnell Springmeyer <parnell@awakesecurity.com>
2018-11-11 18:23:42 -0600

    nixpkgs-17.09 -> nixpkgs-18.09
    Rider: use `callCabal2nix` instead of manually generating the
    `default.nix`.

--- release.nix
+++ release.nix
@@ -1,20 +1,27 @@
 let
 
+  fetchNixpkgs = import ./nix/fetchNixpkgs.nix;
+
+  nixpkgs = fetchNixpkgs {
+    rev          = "def2618c098fad7db233117dc06ce3eb7be93e2d";
+    sha256       = "06hf9rcilvpl7shmmkppaa7c45jnfcsk8850c8zxr369vzlwdrpq";
+    outputSha256 = "1654yc4xyacb28ixsia1i085g7sbvgmil2x7bn1faqp670n42hjr";
+  };
+
   config   = { allowUnfree = true; };
   overlays = [
     (newPkgs: oldPkgs: rec {
 
       haskellPackages = oldPkgs.haskellPackages.override {
         overrides = haskellPackagesNew: haskellPackagesOld: {
-          orgmode-parse = haskellPackagesNew.callPackage ./default.nix { };
+          orgmode-parse = haskellPackagesNew.callCabal2nix "orgmode-parse" ./. { };
         };
       };
 
     })
   ];
 
-  nixpkgs = import ./nix/17_09.nix;
-  pkgs    = import nixpkgs { inherit config overlays; };
+  pkgs = import nixpkgs { inherit config overlays; };
 
   darwinPkgs = import nixpkgs { inherit config overlays; system = "x86_64-darwin"; };
   linuxPkgs  = import nixpkgs { inherit config overlays; system = "x86_64-linux" ; };
