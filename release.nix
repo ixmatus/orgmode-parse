6e230f4bf6dc9b40172201363d918840a5ae69a6
Parnell Springmeyer <parnell@awakesecurity.com>
2018-11-21 11:46:26 -0600

    Updating Nix tooling to support a parameterized compiler argument

--- release.nix
+++ release.nix
@@ -1,33 +1,30 @@
-let
+{ compiler ? "ghc844" }:
 
+let
   fetchNixpkgs = import ./nix/fetchNixpkgs.nix;
 
   nixpkgs = fetchNixpkgs {
-    rev          = "def2618c098fad7db233117dc06ce3eb7be93e2d";
-    sha256       = "06hf9rcilvpl7shmmkppaa7c45jnfcsk8850c8zxr369vzlwdrpq";
-    outputSha256 = "1654yc4xyacb28ixsia1i085g7sbvgmil2x7bn1faqp670n42hjr";
+    rev          = "bedbba61380a4da0318de41fcb790c176e1f26d1";
+    sha256       = "0z4fgh15nz86kxib9ildmh49v6jim6vgbjyla7jbmgdcl0vd9qsg";
+    outputSha256 = "0dxxw2ipa9403nk8lggjsypbr1a9jpb3q4hkjsg89gr5wz26p217";
   };
 
-  config   = { allowUnfree = true; };
   overlays = [
-    (newPkgs: oldPkgs: rec {
-
-      haskellPackages = oldPkgs.haskellPackages.override {
-        overrides = haskellPackagesNew: haskellPackagesOld: {
-          orgmode-parse = haskellPackagesNew.callCabal2nix "orgmode-parse" ./. { };
+    (newPkgs: oldPkgs: {
+      haskell = oldPkgs.haskell // {
+        packages = oldPkgs.haskell.packages // {
+          "${compiler}" = oldPkgs.haskell.packages."${compiler}".override {
+            overrides = (haskellPackagesNew: haskellPackagesOld: {
+              orgmode-parse = haskellPackagesNew.callCabal2nix "orgmode-parse" ./. { };
+            });
+          };
         };
       };
-
     })
   ];
 
-  pkgs = import nixpkgs { inherit config overlays; };
-
-  darwinPkgs = import nixpkgs { inherit config overlays; system = "x86_64-darwin"; };
-  linuxPkgs  = import nixpkgs { inherit config overlays; system = "x86_64-linux" ; };
+  pkgs = import nixpkgs { inherit overlays; };
 
 in
-  { orgmode-parse-linux  =  linuxPkgs.haskellPackages.orgmode-parse;
-    orgmode-parse-darwin = darwinPkgs.haskellPackages.orgmode-parse;
-    orgmode-parse        =       pkgs.haskellPackages.orgmode-parse;
-  }
+
+  { orgmode-parse = pkgs.haskell.packages."${compiler}".orgmode-parse;}
